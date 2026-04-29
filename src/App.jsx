0bf9bc9717f1844a8ebdcd990bcc25976ffac39a
PapireddySatya <satyapapireddy@gmail.com>
2026-04-29 09:48:17 +0530

    features: added About, Home, Projects, Skills, Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+
+import Header from './Header.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <h1>hello</h1>
+    <>
+
+   <Home/>
+   <About/>
+   <projects/>
+   <Skills/>
+   <Contact/>
+   </>
+    
   )
     
 }
-
-export default App
+export default App;
